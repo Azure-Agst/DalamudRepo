6dff34422a9e4a73d1aa383f7aa8c871c9b4565b
Azure-Agst <me@azureagst.dev>
2023-10-17 20:51:30 -0400

    Simplify lol

--- tools/generate_manifest.py
+++ tools/generate_manifest.py
@@ -82,13 +82,8 @@ def main():
     with open("repo.json", "w") as f:
         json.dump(repo_manifest, f, indent=2)
     
-    # Print Successes
+    # Print Success & Exit
     print("Successfully generated repo file!")
-    if os.environ.get("GITHUB_ACTIONS"):
-        with open(os.environ.get("GITHUB_OUTPUT"), "a") as gho:
-            gho.write("::notice::Generated repo file!\n")
-    
-    # Exit
     return 0
 
 if __name__ == "__main__":
